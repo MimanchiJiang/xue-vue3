4208082d358fbb6d77f664f9782800845cea04fd
JHuaWei <2237268069@qq.com>
2022-10-05 15:33:16 +0800

    初始化路由

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>hi</div>
-  <frank></frank>
+  <div>导航栏 | <router-link to="/">Frank1</router-link> | <router-link to="/xxx">Frank2</router-link>
+  </div>
+  <hr />
+  <router-view />
 </template>
 
 <script>
-import frank from './components/frank.vue'
 
 export default {
   name: 'App',
-  components: {
-    frank
-  }
 }
 </script>
