e9152700150df0273eb53a939b3f0aee43beb39d
JHuaWei <2237268069@qq.com>
2022-10-07 12:59:46 +0800

    button 支持 size和level

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,14 +1,37 @@
 <template>
-    <button class="xue-button" :class="`xue-theme-${theme}`">
+    <button class="xue-button" :class="classes">
         <slot />
     </button>
 </template>
 <script lang="ts">
+import { computed } from 'vue';
+
 export default {
     props: {
         theme: {
             type: String,
             default: 'button'
+        },
+        size: {
+            type: String,
+            default: "normal"
+        },
+        level: {
+            type: String,
+            default: "normal"
+        }
+    },
+    setup(props) {
+        const { theme, size, level } = props
+        const classes = computed(() => {
+            return {
+                [`xue-theme-${theme}`]: theme,
+                [`xue-size-${size}`]: size,
+                [`xue-level-${level}`]: level
+            }
+        })
+        return {
+            classes
         }
     }
 }
@@ -19,6 +42,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 
 .xue-button {
     box-sizing: border-box;
@@ -34,6 +58,7 @@ $radius: 4px;
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
 
     &+& {
         margin-left: 8px;
@@ -51,6 +76,75 @@ $radius: 4px;
 
     &::-moz-focus-inner {
         border: 0;
+    }
+
+    &.xue-size-big {
+        font-size: 24px;
+        height: 48px;
+        padding: 0 16px;
+    }
+
+    &.xue-size-small {
+        font-size: 12px;
+        height: 20px;
+        padding: 0 4px;
+    }
+
+    &.xue-theme-button {
+        &.xue-level-main {
+            background: $blue;
+            color: white;
+            border-color: $blue;
+
+            &:hover,
+            &:focus {
+                background: darken($blue, 10%);
+                border-color: darken($blue, 10%);
+            }
+        }
+
+        &.xue-level-danger {
+            background: $red;
+            border-color: $red;
+            color: white;
+
+            &:hover,
+            &:focus {
+                background: darken($red, 10%);
+                border-color: darken($red, 10%);
+            }
+        }
+    }
+
+    &.xue-theme-link {
+        &.xue-level-danger {
+            color: $red;
+
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
+    }
+
+    &.xue-theme-text {
+        &.xue-level-main {
+            color: $blue;
+
+            &:hover,
+            &:focus {
+                color: darken($blue, 10%);
+            }
+        }
+
+        &.xue-level-danger {
+            color: $red;
+
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
     }
 }
 </style>
