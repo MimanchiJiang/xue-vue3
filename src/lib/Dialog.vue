5dde6febb33d653bafc12d8a28cbd92c516dd07e
JHuaWei <2237268069@qq.com>
2022-10-08 16:16:26 +0800

    使用teleport 将dialog移至body下面

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,20 +1,23 @@
 <template>
     <template v-if="visible">
-        <div class="xue-dialog-overlay" @click="onClickOverlay"></div>
-        <div class="xue-dialog-wrapper">
-            <div class="xue-dialog">
-                <header>
-                    <slot name="title" /> <span class="xue-dialog-close" @click="close"></span>
-                </header>
-                <main>
-                    <slot name="content" />
-                </main>
-                <footer>
-                    <Button level="main" @click="ok">OK</Button>
-                    <Button @click="cancel">Cancel</Button>
-                </footer>
+        <Teleport to="body">
+            <div class="xue-dialog-overlay" @click="onClickOverlay"></div>
+            <div class="xue-dialog-wrapper">
+                <div class="xue-dialog">
+                    <header>
+                        <slot name="title" /> <span class="xue-dialog-close" @click="close"></span>
+                    </header>
+                    <main>
+                        <slot name="content" />
+                    </main>
+                    <footer>
+                        <Button level="main" @click="ok">OK</Button>
+                        <Button @click="cancel">Cancel</Button>
+                    </footer>
+                </div>
             </div>
-        </div>
+        </Teleport>
+
     </template>
 
 </template>
