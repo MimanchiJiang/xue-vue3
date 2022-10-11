bfdd8100e7bdbd66d7d77e78c239638e5bec75db
JHuaWei <2237268069@qq.com>
2022-10-11 20:54:21 +0800

    Tab css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-        <component v-for="(c,index) in defaults" :is="c" :key="index" />
+    <div class="xue-tabs">
+        <div class="xue-tabs-nav">
+            <div class="xue-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+        </div>
+        <div class="xue-tabs-content">
+            <component class="xue-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -25,3 +29,34 @@ export default {
     }
 }
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.xue-tabs {
+    &-nav {
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            &.selected {
+                color: $blue;
+            }
+        }
+    }
+
+    &-content {
+        padding: 8px 0;
+    }
+}
+</style>
