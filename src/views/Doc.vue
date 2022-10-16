65a57279ed3bee5e95e5afb7f8451e11e0185832
JHuaWei <2237268069@qq.com>
2022-10-16 11:28:14 +0800

    高亮优化

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -53,10 +53,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.router-link-active {
-    text-decoration: underline;
-}
-
 .layout {
     display: flex;
     flex-direction: column;
@@ -94,7 +90,7 @@ export default {
 aside {
     background: lightblue;
     width: 130px;
-    padding: 16px;
+    padding: 16px 0;
     position: fixed;
     top: 0;
     left: 0;
@@ -103,11 +99,21 @@ aside {
 
     >h2 {
         margin-bottom: 4px;
+        padding: 0 16px;
     }
 
     >ol {
         >li {
-            padding: 4px 0;
+            >a {
+                display: block;
+                padding: 4px 16px;
+            }
+
+            .router-link-active {
+                background: white;
+
+            }
+
         }
     }
 }
